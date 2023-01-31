ba2b4716bf5dbb51d62eac4db29f8a48e26422ae
skip-rat <skip-rat@users.noreply.github.com>
2023-01-31 15:00:19 +0000

    Update exercise001.js
    Added async/await & try catch to the solution

--- exercise001.js
+++ exercise001.js
@@ -1,15 +1,28 @@
-const promise = new Promise((resolve, reject) => {
-	// Generate random number between 0 and 9
-	const randomInt = Math.floor(Math.random() * 10);
+  const somePromiseObject = () => {
+	
+	return new Promise((resolve, reject) => {
+		// Generate random number between 0 and 9
+		const randomInt = Math.floor(Math.random() * 10);
+	
+		if (randomInt % 2 === 0) {
+			// Success
+			setTimeout(() => resolve('done'), 2000);
+		} else {
+			// Failure
+			setTimeout(() => reject('error'), 2000);
+		}
+	});
+}
 
-	if (randomInt % 2 === 0) {
-		// Success
-		setTimeout(() => resolve('done'), 2000);
-	} else {
-		// Failure
-		setTimeout(() => reject('error'), 2000);
+const getPromiseResponse = async () =>
+{
+	try {
+		const result = await somePromiseObject();
+		console.log(result);
+  	} catch (error) {
+    	console.log(error)
 	}
-}); 
+}
 
 // Your solution(s) to exercise001 here!
 
